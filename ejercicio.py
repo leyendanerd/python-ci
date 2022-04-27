9afed0124334eee670ad10c7b54d2087749f9871
leyendanerd <leyendanerd@gmail.com>
2022-04-26 23:00:14 -0400

    error

--- ejercicio.py
+++ ejercicio.py
@@ -24,7 +24,7 @@ class Estudiante(Persona):
         super().imprimirnombrelargo()
         return f'Su ano de graduacion es {self.graduacion}'
 
-Estudiante1 = Estudiante("Jose", "lal uz", 2022, "Programador")
+Estudiante = Estudiante("Jose", "lal uz", 2022, "Programador")
 #print(Estudiante1.apellido)
 
 #Estudiante.bienvenida()
